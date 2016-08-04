8de72f86ac06d4163fc4ded52815f54ffcb2aa2e
Badrinath <abadrinath@paypalcorp.com>
2016-08-04 11:56:25 +0530

    Added method to fetch from file

--- Anagrams.java
+++ Anagrams.java
@@ -1,7 +1,23 @@
-class import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Anagrams{
-     
+
+     List<String> getWordsFromFile(String path) throws IOException {
+         FileReader file = new FileReader(path);
+         BufferedReader reader = new BufferedReader(file);
+         List<String> lines = new ArrayList<>();
+         String singleLine = "";
+         while ((singleLine = reader.readLine()) != null) {
+             lines.add(singleLine);
+         }
+         return lines;
+     }
      
      public static String sortString (String a) {
         char[] chars = a.toCharArray();
@@ -12,7 +28,6 @@ public class Anagrams{
      }
      
      public static void main(String []args){
-        
         
         
      }
